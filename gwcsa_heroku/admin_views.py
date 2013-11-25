78c929198f95d4dce5445d49f45a1571312a0f8e
Alexis Lin <alexislin95@gmail.com>
2013-11-25 17:47:13 -0500

    show share counts in admin members page

--- gwcsa_heroku/admin_views.py
+++ gwcsa_heroku/admin_views.py
@@ -8,6 +8,15 @@ from gwcsa_heroku.models import *
 from gwcsa_heroku.request_util import *
 from gwcsa_heroku.util import *
 
+
+SHARE_COUNT_QUERY = """
+    SELECT gwcsa_heroku_share.quantity
+      FROM gwcsa_heroku_share
+     WHERE gwcsa_heroku_share.member_id = gwcsa_heroku_member.id
+       AND gwcsa_heroku_share.content = '%s'
+       AND gwcsa_heroku_share.frequency = '%s'
+"""
+
 @handle_view_exception
 @login_required
 def members(request):
@@ -54,7 +63,18 @@ def members(request):
 '''
 
 
-    members = Member.objects.annotate(shift_count=Count("memberworkshift")).order_by("day", "last_name")
+    members = Member.objects.annotate(
+        shift_count=Count("memberworkshift")
+    ).extra(select={
+        "weekly_veggie_count": SHARE_COUNT_QUERY % (VEGETABLES, WEEKLY),
+        "biweekly_veggie_count": SHARE_COUNT_QUERY % (VEGETABLES, BIWEEKLY),
+        "weekly_fruit_count": SHARE_COUNT_QUERY % (FRUIT, WEEKLY),
+        "biweekly_fruit_count": SHARE_COUNT_QUERY % (FRUIT, BIWEEKLY),
+        "weekly_egg_count": SHARE_COUNT_QUERY % (EGGS, WEEKLY),
+        "biweekly_egg_count": SHARE_COUNT_QUERY % (EGGS, BIWEEKLY),
+        "weekly_flower_count": SHARE_COUNT_QUERY % (FLOWERS, WEEKLY),
+        "biweekly_flower_count": SHARE_COUNT_QUERY % (FLOWERS, BIWEEKLY),
+    }).order_by("day", "last_name")
     return render_to_response("admin_members.html",
         RequestContext(request, {
             "current_season": CURRENT_SEASON,
