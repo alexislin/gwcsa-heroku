64d5cb3a78214e0d3771c8035789a94ad17ee200
Alexis Lin <alexislin95@gmail.com>
2013-11-25 13:24:18 -0500

    add shift count to admin members page

--- gwcsa_heroku/admin_views.py
+++ gwcsa_heroku/admin_views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
@@ -9,7 +10,7 @@ from gwcsa_heroku.request_util import *
 @handle_view_exception
 @login_required
 def members(request):
-    members = Member.objects.all()
+    members = Member.objects.annotate(shift_count=Count("memberworkshift"))
     return render_to_response("admin_members.html",
         RequestContext(request, {
             "current_season": CURRENT_SEASON,
