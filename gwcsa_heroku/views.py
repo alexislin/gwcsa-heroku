4deb0d8b75b28ce77f9287a90a17e8ad3afeabcb
Alexis Lin <alexislin95@gmail.com>
2015-03-12 13:04:39 -0400

    oops, dd57c5792aab18f604bb73171569f9ac36a62466

--- gwcsa_heroku/views.py
+++ gwcsa_heroku/views.py
@@ -3,6 +3,7 @@ import logging
 
 from datetime import datetime
 
+from django.conf import settings
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
@@ -55,7 +56,7 @@ def workshift_selection(request):
         # ensure workshift selection doesn't fail just because the email
         # doesn't send (can happen when SendGrid credits run out)
         try:
-            if not DEBUG:
+            if not settings.DEBUG:
                 send_workshift_confirmation_email(member)
         except Exception:
             stack_trace = traceback.format_exc()
